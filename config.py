fb642235fc8cd157ff79300418e42e42f487bda9
blandk77 <alexags1983@gmail.com>
2025-04-09 22:52:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "26728872")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "96985c2aaea6c75408528909b7e18879") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7898251858:AAGec8iPruS_VU1v20ComTH4ZNQ99WM66t8") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7898251858:AAEjckK4FBsAj-xzbd2btZR3EKQh6GSmnhs") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', 'The_TGguy') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
     DB_URL  = os.environ.get("DB_URL", "mongodb+srv://Telegram_Guy:I6AfG9KKBJ5397xF@botstore.t3cuf.mongodb.net/?retryWrites=true&w=majority&appName=Botstore")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","sjjabaosnwkjzjanx") 
+    DB_NAME  = os.environ.get("DB_NAME","sjjabaosnwkzjanx") 
 
     # Other Configs 
     ADMIN = int(os.environ.get("ADMIN", "1705634892")) # ⚠️ Required
